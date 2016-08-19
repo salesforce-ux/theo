a875806cf17c18709e9758d39341805acd26b9ac
jina <jina@sushiandrobots.com>
2016-08-18 17:37:05 -0700

    theme updates

--- src/props/formats/html.jsx
+++ src/props/formats/html.jsx
@@ -20,7 +20,7 @@ let upperfirst = require('lodash/upperFirst');
 let Style = React.createClass({
   render() {
     return (
-      <style>
+      <style type="text/css">
         {require('./html.style')}
       </style>
     );
@@ -30,9 +30,7 @@ let Style = React.createClass({
 let ExampleRow = React.createClass({
   render() {
     return (
-      <td className="example" {...this.props}>
-        {this.props.children}
-      </td>
+      <td {...this.props}>{this.props.children}</td>
     );
   }
 });
@@ -47,12 +45,14 @@ let Styleguide = React.createClass({
 
   renderRowHeader(id, heading) {
     return (
-      <tr key={`${id}-header`} id={id}>
-        <th>{heading}</th>
-        <th>Value</th>
-        <th>Examples</th>
-        <th>Usage</th>
-      </tr>
+      <thead>
+        <tr key={`${id}-header`} id={id}>
+          <th scope="column">{heading}</th>
+          <th scope="column">Value</th>
+          <th scope="column">Examples</th>
+          <th scope="column">Usage</th>
+        </tr>
+      </thead>
     );
   },
 
@@ -60,73 +60,36 @@ let Styleguide = React.createClass({
     let name = camelCase(prop.name);
     return (
       <tr key={`${prop.name}-row`}>
-        <td>
+        <th scope="row">
           <code>{name}</code>
-        </td>
+        </th>
         <td>
           <code>{prop.value}</code>
         </td>
         {example}
-        <td className="description">
-          {prop.comment}
-        </td>
+        <td>{prop.comment}</td>
       </tr>
     )
   },
 
-  renderBackgroundColor(props) {
-    return props.map(prop => {
-      let example = <ExampleRow style={{backgroundColor:prop.value, border:'1px solid #f2f2f2'}} />
-      return this.renderRow(prop, example);
-    })
-  },
-
-  renderTextColor(props) {
-    return props.map(prop => {
-      let example = (
-        <ExampleRow style={{color:prop.value}}>
-          The quick brown fox jumps over the lazy dog.
-        </ExampleRow>
-      );
-      return this.renderRow(prop, example);
-    })
-  },
-
-  renderGradient(props) {
-    return props.map(prop => {
-      let example = <ExampleRow style={{background:prop.value}} />
-      return this.renderRow(prop, example);
-    })
-  },
-
-  renderBorderColor(props) {
-    return props.map(prop => {
-      let example = <ExampleRow style={{border:`2px solid ${prop.value}`}} />
-      return this.renderRow(prop, example);
-    })
-  },
-
-  renderBorderStyle(props) {
-    return props.map(prop => {
-      let example = <ExampleRow style={{border:prop.value}} />
-      return this.renderRow(prop, example);
-    })
-  },
-
-  renderHrColor(props) {
+  renderSpacing(props) {
     return props.map(prop => {
       let example = (
         <ExampleRow>
-          <hr style={{borderTopColor:prop.value}} />
+          <div className="metric-box" style={{width:prop.value, height:prop.value}} />
         </ExampleRow>
       );
       return this.renderRow(prop, example);
     })
   },
 
-  renderDropShadow(props) {
+  renderSizing(props) {
     return props.map(prop => {
-      let example = <ExampleRow style={{boxShadow:prop.value}} />
+      let example = (
+        <ExampleRow>
+          <div className="metric-box" style={{width:prop.value, height:prop.value}} />
+        </ExampleRow>
+      );
       return this.renderRow(prop, example);
     })
   },
@@ -135,7 +98,22 @@ let Styleguide = React.createClass({
     return props.map(prop => {
       let example = (
         <ExampleRow>
-          <div style={{fontFamily:prop.value}}>Aa</div>
+          <div style={{fontFamily:prop.value}}>
+            The quick brown fox jumps over the lazy dog.
+          </div>
+        </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderFontStyle(props) {
+    return props.map(prop => {
+      let example = (
+        <ExampleRow>
+          <div style={{fontStyle:prop.value}}>
+            The quick brown fox jumps over the lazy dog.
+          </div>
         </ExampleRow>
       );
       return this.renderRow(prop, example);
@@ -146,7 +124,9 @@ let Styleguide = React.createClass({
     return props.map(prop => {
       let example = (
         <ExampleRow>
-          <div style={{fontWeight:prop.value}}>Aa</div>
+          <div style={{fontWeight:prop.value}}>
+            The quick brown fox jumps over the lazy dog.
+          </div>
         </ExampleRow>
       );
       return this.renderRow(prop, example);
@@ -166,26 +146,15 @@ let Styleguide = React.createClass({
     })
   },
 
-  renderTime(props) {
-    return props.map(prop => {
-      let example = (
-        <ExampleRow />
-      );
-      return this.renderRow(prop, example);
-    })
-  },
-
   renderLineHeight(props) {
     return props.map(prop => {
       let vHeight = !isNaN(prop.value) ? `${prop.value}em` : prop.value;
       let example = (
         <ExampleRow>
           <div className="line-height-example" style={{lineHeight:prop.value, backgroundSize:`100% ${vHeight}`}}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec elementum odio et lacus rutrum molestie. Nunc arcu enim, elementum id feugiat at,
-            venenatis quis erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos. Mauris sit amet commodo velit. In hac habitasse platea dictumst.
-            Sed ac velit dolor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            elementum odio et lacus rutrum molestie. Nunc arcu enim, elementum
+            id feugiat at, venenatis quis erat.
           </div>
         </ExampleRow>
       );
@@ -193,11 +162,38 @@ let Styleguide = React.createClass({
     })
   },
 
-  renderSpacing(props) {
+  renderFontFamily(props) {
     return props.map(prop => {
       let example = (
         <ExampleRow>
-          <div className="metric-box" style={{width:prop.value, height:prop.value}} />
+          <div style={{fontFamily:prop.value}}>
+            The quick brown fox jumps over the lazy dog.
+          </div>
+        </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBorderStyle(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{border:prop.value}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBorderColor(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{border:`2px solid ${prop.value}`}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderHrColor(props) {
+    return props.map(prop => {
+      let example = (
+        <ExampleRow>
+          <hr style={{borderTopColor:prop.value}} />
         </ExampleRow>
       );
       return this.renderRow(prop, example);
@@ -206,10 +202,88 @@ let Styleguide = React.createClass({
 
   renderRadius(props) {
     return props.map(prop => {
+      let name = camelCase(prop.name);
+      let example = (
+        <ExampleRow>
+          <div className={`radius-box ${name}`} style={{borderRadius:prop.value}} />
+        </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBorderRadius(props) {
+    return props.map(prop => {
       let example = (
         <ExampleRow>
           <div className="radius-box" style={{borderRadius:prop.value}} />
         </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBackgroundColor(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{backgroundColor:prop.value, border:'1px solid #f6f6f6'}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderGradient(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{background:prop.value}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBackgroundGradient(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{backgroundImage:prop.value}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderDropShadow(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{boxShadow:prop.value}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderBoxShadow(props) {
+    return props.map(prop => {
+      let example = <ExampleRow style={{boxShadow:prop.value}} />
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderTextColor(props) {
+    return props.map(prop => {
+      let example = (
+        <ExampleRow style={{backgroundColor:'#f6f6f6', color:prop.value}}>
+          The quick brown fox jumps over the lazy dog.
+        </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderTextShadow(props) {
+    return props.map(prop => {
+      let example = (
+        <ExampleRow style={{textShadow:prop.value}}>
+          The quick brown fox jumps over the lazy dog.
+        </ExampleRow>
+      );
+      return this.renderRow(prop, example);
+    })
+  },
+
+  renderTime(props) {
+    return props.map(prop => {
+      let example = (
+        <ExampleRow />
       );
       return this.renderRow(prop, example);
     })
@@ -229,50 +303,63 @@ let Styleguide = React.createClass({
     if (!props) { return null; }
     let name = upperfirst(camelCase(type));
     let render = typeof fn === 'function' ? fn : this[`render${name}`];
-    return [
-      this.renderRowHeader(type, heading),
-      render.call(this, props)
-    ];
+    return (
+      <section>
+        <table>
+          {this.renderRowHeader(type, heading)}
+          <tbody>
+            {render.call(this, props)}
+          </tbody>
+        </table>
+        <hr />
+      </section>
+    );
   },
 
   render(json) {
     return (
-      <html>
+      <html lang="en">
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <title>Design Tokens</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" />
           <Style />
         </head>
         <body>
           <header role="banner" className="banner">
             <div className="container">
-              <h1>Design Attribute Guide</h1>
+              <h1>Design Tokens</h1>
             </div>
           </header>
+
           <div className="container">
             <main role="main">
-              <table>
-                <tbody>
-                  {this.renderSection('text-color', 'Text Colors')}
-                  {this.renderSection('background-color', 'Background Colors')}
-                  {this.renderSection('gradient', 'Gradients')}
-                  {this.renderSection('border-color', 'Border Colors')}
-                  {this.renderSection('border-style', 'Border Styles')}
-                  {this.renderSection('hr-color', 'Horizontal Rule Colors')}
-                  {this.renderSection('drop-shadow', 'Drop Shadows')}
-                  {this.renderSection('inner-shadow', 'Inner Drop Shadows', this.renderDropShadow)}
-                  {this.renderSection('font', 'Fonts')}
-                  {this.renderSection('font-weight', 'Font Weights')}
-                  {this.renderSection('font-size', 'Font Sizes')}
-                  {this.renderSection('line-height', 'Line Heights')}
-                  {this.renderSection('spacing', 'Spacing')}
-                  {this.renderSection('radius', 'Radius')}
-                  {this.renderSection('time', 'Time')}
-                  {this.renderSection('media-query', 'Media Queries')}
-                </tbody>
-              </table>
+              {this.renderSection('sizing', 'Sizing')}
+              {this.renderSection('spacing', 'Spacing')}
+              {this.renderSection('font', 'Fonts')}
+              {this.renderSection('font-style', 'Font Styles')}
+              {this.renderSection('font-weight', 'Font Weights')}
+              {this.renderSection('font-size', 'Font Sizes')}
+              {this.renderSection('line-height', 'Line Heights')}
+              {this.renderSection('font-families', 'Font Families')}
+              {this.renderSection('border-style', 'Border Styles')}
+              {this.renderSection('border-color', 'Border Colors')}
+              {this.renderSection('radius', 'Radius')}
+              {this.renderSection('border-radius', 'Border Radii')}
+              {this.renderSection('hr-color', 'Horizontal Rule Colors')}
+              {this.renderSection('background-color', 'Background Colors')}
+              {this.renderSection('gradient', 'Gradients')}
+              {this.renderSection('background-gradient', 'Background Gradients')}
+              {this.renderSection('drop-shadow', 'Drop Shadows')}
+              {this.renderSection('box-shadow', 'Box Shadows')}
+              {this.renderSection('inner-shadow', 'Inner Drop Shadows', this.renderDropShadow)}
+              {this.renderSection('box-shadow', 'Box Shadows')}
+              {this.renderSection('text-color', 'Text Colors')}
+              {this.renderSection('text-shadow', 'Text Shadows')}
+              {this.renderSection('time', 'Time')}
+              {this.renderSection('media-query', 'Media Queries')}
             </main>
           </div>
         </body>
