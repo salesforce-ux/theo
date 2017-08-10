c43a77506e91917cb660d345cfd72d4130581e22
Nick Balestra <nbalestra@opentable.com>
2017-08-10 18:36:06 +0100

    lint

--- bin/scripts/build.js
+++ bin/scripts/build.js
@@ -1,21 +1,21 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
-const theo = require('../../lib');
-const fs = require('fs-extra');
-const path = require('path');
-const each = require('async/each');
+const theo = require('../../lib')
+const fs = require('fs-extra')
+const path = require('path')
+const each = require('async/each')
 
-module.exports = function build(options, cb) {
-  const {formats, packagePath, distPath, test, fileName, source} = options;
+module.exports = function build (options, cb) {
+  const {formats, packagePath, distPath, test, fileName, source} = options
 
   if (distPath !== '.' && !test) {
-    fs.emptyDirSync(path.join(packagePath, distPath));
-    console.log();
+    fs.emptyDirSync(path.join(packagePath, distPath))
+    console.log()
   }
 
   each(formats, (format, callback) => {
-    const outputFile = path.join(distPath, `${fileName}.${format}`);
+    const outputFile = path.join(distPath, `${fileName}.${format}`)
     theo.convert({
       transform: {
         type: 'raw',
@@ -28,10 +28,10 @@ module.exports = function build(options, cb) {
     .then(data => {
       if (!test) {
         fs.outputFile(path.join(packagePath, outputFile), data)
-          .then(()=> console.log(`✏️  ${format} tokens created at "${path.basename(packagePath)}/${outputFile}"`));
-        callback();
+          .then(() => console.log(`✏️  ${format} tokens created at "${path.basename(packagePath)}/${outputFile}"`))
+        callback()
       } else {
-        console.log(`✓ ${path.basename(packagePath)}/${outputFile} compiled correctly`);
+        console.log(`✓ ${path.basename(packagePath)}/${outputFile} compiled correctly`)
         callback()
       }
     })
--- bin/theo.js
+++ bin/theo.js
@@ -2,23 +2,22 @@
 // Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 // Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
-const argv = require('optimist').argv;
-const build = require('./scripts/build');
-
+const argv = require('optimist').argv
+const build = require('./scripts/build')
 
 const options = {
-  formats : argv._,
-  packagePath : argv.path || argv.p || process.cwd(),
-  distPath : argv.dist || argv.d || '.',
-  fileName : argv.output || argv.o || 'token',
-  source : argv.src || argv.s || 'token.yml',
-  test : argv.test || argv.t || false
-};
+  formats: argv._,
+  packagePath: argv.path || argv.p || process.cwd(),
+  distPath: argv.dist || argv.d || '.',
+  fileName: argv.output || argv.o || 'token',
+  source: argv.src || argv.s || 'token.yml',
+  test: argv.test || argv.t || false
+}
 
 build(options, error => {
   if (error) {
-    console.log(`💩  Oups, something went wrong: ${error}`);
-    return process.exit(1);
+    console.log(`💩  Oups, something went wrong: ${error}`)
+    return process.exit(1)
   }
-  return process.exit(0);
-});
+  return process.exit(0)
+})
