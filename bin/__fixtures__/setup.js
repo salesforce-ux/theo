347cd985391999570b8baf27c3761af43b3ae20f
tomg <tom.germeau@gmail.com>
2018-01-27 20:48:45 -0500

    Add test for --setup
    and cleanup setup.js

--- bin/__fixtures__/setup.js
+++ bin/__fixtures__/setup.js
@@ -1,18 +1,9 @@
 module.exports = theo => {
   theo.registerFormat("array.js", result => {
-    // "result" is an Immutable.Map
-    // https://facebook.github.io/immutable-js/
-    return `
-      module.exports = [
-        ${result
-          .get("props")
-          .map(
-            prop => `
-          ['${prop.get("name")}', '${prop.get("value")}'],
-        `
-          )
-          .toJS()}
-      ]
-    `;
+    let items = result
+      .get("props")
+      .map(prop => `\n  ['${prop.get("name")}', '${prop.get("value")}'],`)
+      .toJS();
+    return `module.exports = [${items}\n]`;
   });
 };
